21e092cb17b3277b1b700d72d2a03891c3dcde43
elin9 <elin9@u.rochester.edu>
2014-10-09 16:18:30 -0600

    logout implementation

---
+++ cgi-bin/logout.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+
+import cgi
+import cgitb
+import sqlite3
+cgitb.enable()
+
+form = cgi.FieldStorage()
+
+conn = sqlite3.connect('BESTOS_DATABASE.db')
+
+def main():
+	sID = form['sid'].value
+	print """Content-type: text/html
+
+<html><head><title>Logout</title></head><body>
+<p>You are now logged out. Return to <a href = "http://elin9.rochestercs.org/">home</a>.</p>
+</body></html>"""
+	logout(conn,sID)
+
+def logout(conn, sID):
+	c = conn.cursor()
+	c.execute('update users set sessionID=0 where sessionID=?;',(sID,))
+	conn.commit()
+	conn.close()
+	
+main()
